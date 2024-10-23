37098fa667a47a66cbf271251ccfb35e44c4f298
Axuata <axuata@gmail.com>
2024-10-23 17:33:25 +0900

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,16 @@ export default defineNuxtConfig({
     },
     display: 'swap',
   },
+  app:{
+    head: {
+      title: "Your website name here",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/your/favicon/path" },
+      ],
+    },
+  },
 })
